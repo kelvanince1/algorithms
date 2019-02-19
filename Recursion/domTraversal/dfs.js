3a629a33fbcdc099c8111882c19ac0c77f6aee5c
Kelvan Ince <incekelvan@gmail.com>
2019-02-19 12:48:10 -0500

    Add DFS traversal on DOM

---
+++ Recursion/domTraversal/dfs.js
@@ -0,0 +1,46 @@
+function traverseDF(fn) {
+    const parent = document.body.children[0];
+    const result = [parent];
+
+    while (result.length) {
+        const node = result.shift();
+
+        result.unshift(...node.children);
+        fn(node);
+    }
+
+	return result;
+}
+
+
+const arr = [];
+
+this.traverseDF(node => {
+    arr.push(node.classname);
+});
+
+// This assumes the following index.html file:
+
+// <!DOCTYPE html>
+// <html lang="en" dir="ltr">
+//   <head>
+//     <meta charset="utf-8">
+//     <title></title>
+//   </head>
+//   <body>
+//     <div class="letters">
+//       <div class="a">
+//         <span class="aa">
+//         </span>
+//         <span class="aaa">
+//         </span>
+//       </div>
+//       <div class="b">
+//         <span class="bb">
+//         </span>
+//         <span class="bbb">
+//         </span>
+//       </div>
+//     </div>
+//   </body>
+// </html>
