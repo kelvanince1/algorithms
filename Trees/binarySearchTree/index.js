76212327e791d7045af4d0c0c5f16455406c46cd
Kelvan Ince <incekelvan@gmail.com>
2019-09-23 10:14:43 -0400

    Add remove to bst

--- Trees/binarySearchTree/index.js
+++ Trees/binarySearchTree/index.js
@@ -60,4 +60,72 @@ class BinarySearchTree {
 
         return false;
     }
+
+    remove(value) {
+        if (!this.root) {
+            return false;
+        }
+
+        let current = this.root;
+        let parent = null;
+
+        while (current) {
+            if (value < current.value) {
+                parent = current;
+                current = current.left;
+            } else if (value > current.value) {
+                parent = current;
+                current = current.right;
+            } else if (value === current.value) {
+                if (current.right === null) {
+                    if (parent === null) {
+                        this.root = current.left;
+                    } else {
+                        if (current.value < parent.value) {
+                            parent.left = current.left;
+                        } else if (current.value > parent.value) {
+                            parent.right = current.left;
+                        }
+                    }
+                } else if (current.right.left === null) {
+                    if (parent === null) {
+                        this.root = current.left;
+                    } else {
+                        current.right.left = current.left;
+
+                        if (current.value < parent.value) {
+                            parent.left = current.right;
+                        } else if (current.value > parent.value) {
+                            parent.right = current.right;
+                        }
+                    }
+                } else {
+
+                    //find the Right child's left most child
+                    let leftmost = currentNode.right.left;
+                    let leftmostParent = currentNode.right;
+                    while(leftmost.left !== null) {
+                      leftmostParent = leftmost;
+                      leftmost = leftmost.left;
+                    }
+                    
+                    //Parent's left subtree is now leftmost's right subtree
+                    leftmostParent.left = leftmost.right;
+                    leftmost.left = currentNode.left;
+                    leftmost.right = currentNode.right;
+          
+                    if(parentNode === null) {
+                      this.root = leftmost;
+                    } else {
+                      if(currentNode.value < parentNode.value) {
+                        parentNode.left = leftmost;
+                      } else if(currentNode.value > parentNode.value) {
+                        parentNode.right = leftmost;
+                      }
+                    }
+                  }
+                  return true;
+            }
+        }
+    }
 }
