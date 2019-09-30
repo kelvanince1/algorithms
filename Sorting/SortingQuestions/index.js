6373d7a97199fc803dfc2ab2f13f3a2115c25b2e
Kelvan Ince <incekelvan@gmail.com>
2019-09-30 14:09:24 -0400

    Add sorting algorithms

---
+++ Sorting/SortingQuestions/index.js
@@ -0,0 +1,25 @@
+// Which sorting algorithm would be most appropriate for:
+
+// 1) Sort ten schools around house
+//  ---> Insertion
+
+// 2) eBay sorts listings by current bid amount
+// ---> Radix or Counting sort
+
+// 3) Sort scores on ESPN
+// ---> Quick
+
+// 4) Massive database (can't fit all into memory)
+// ---> Merge
+
+// 5) Almost sorted Udemy review data and add 2 new reviews
+//  ---> Insertion
+
+// 6) Temperature records for past 50 years in Canada
+//  ---> Quick
+
+// 7) Large username database. Data is random.
+//  ---> Quick
+
+// 8) Teach sorting for first time
+//  ---> Bubble
