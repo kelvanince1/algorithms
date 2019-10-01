9b5a8e564d052e1885d472da907fdbcdc9c478d8
Kelvan Ince <incekelvan@gmail.com>
2019-10-01 16:54:29 -0400

    Add BFS and DFS notes

---
+++ Searching/SearchingQuestions/index.js
@@ -0,0 +1,19 @@
+// Which search should be used for:
+
+// 1) If the tree is very deep and solutions are rare
+// ---> BFS
+
+// 2) If the tree is very wide
+//  ---> DFS
+
+// 3) If solutions are frequent but located deep in the tree
+//  ---> DFS
+
+// 4) Determining if a path exists between two nodes
+//  ---> DFS
+
+// 5) If the solution is not far from the root of the tree
+//  ---> BFS
+
+// 6) Finding the shortest path
+// ---> BFS
