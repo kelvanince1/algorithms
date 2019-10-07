be9692860f70eb6e6b7ad0f965ff8bed25d7043b
Kelvan Ince <incekelvan@gmail.com>
2019-10-07 16:09:48 -0400

    Add memoization function

---
+++ DynamicProgramming/Memoization/index.js
@@ -0,0 +1,18 @@
+function memoizedAddTo80(n) {
+    const cache = {};
+
+    return function(n) {
+        if (n in cache) {
+            return cache[n];
+        } else {
+            console.log('Long');
+            cache[n] = n + 80;
+            return cache[n];
+        }
+    }
+}
+
+const memoized = memoizedAddTo80();
+
+console.log(memoized(5));
+console.log(memoized(5));
