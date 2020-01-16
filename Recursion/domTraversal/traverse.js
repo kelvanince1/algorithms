44cf3ad1da1d64f24198b73c6ba90078957ffb71
Kelvan Ince <incekelvan@gmail.com>
2020-01-15 21:40:51 -0500

    Add sibling traversals

--- Recursion/domTraversal/traverse.js
+++ Recursion/domTraversal/traverse.js
@@ -5,6 +5,7 @@ console.log(header);
 console.log(headerParent);
 
 var content = document.getElementById('letters');
+var middle = document.getElementsByClassName('aaa');
 
 var contentChildren = content.children;
 var contentChildNodes = content.childNodes;
@@ -19,3 +20,16 @@ console.log('FIRST CHILD!!', contentFirstChild);
 console.log('LAST CHILD!!!', contentLastChild);
 console.log('LAST CHILD ELE!!!', contentLastChildEl);
 console.log('FIRST CHILD ELE', contentFirstChildEl);
+
+console.log(middle);
+
+var contentPrevNode = content.previousSibling;
+var contentNextNode = content.nextSibling;
+var contentPrevEl = content.previousElementSibling;
+var contentNextEl = content.nextElementSibling;
+
+console.log('PREV SIB', contentPrevNode);
+console.log('NEXT SIB', contentNextNode);
+console.log('PREV ELE', contentPrevEl);
+console.log('NEXT ELE', contentNextEl);
+
