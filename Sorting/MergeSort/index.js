ebe6c453aa5cf0b5086b2ffd5cb85ff8cc3da9b5
Kelvan Ince <incekelvan@gmail.com>
2020-05-25 15:36:01 -0400

    Add Merge sort algorithm

--- Sorting/MergeSort/index.js
+++ Sorting/MergeSort/index.js
@@ -4,18 +4,34 @@ function mergeSort (array) {
   if (array.length === 1) {
     return array
   }
+
+  if (array.length === 2) {
+    return array[0] > array[1] ? [array[1], array[0]] : array;
+  }
   // Split Array in into right and left
 
-  return merge(
-    mergeSort(left),
-    mergeSort(right)
-  )
+  const middle = Math.floor(array.length / 2);
+  const leftArr = array.slice(0, middle);
+  const rightArr = array.slice(middle);
+
+  const leftSortedArr = mergeSort(leftArr);
+  const rightSortedArr = mergeSort(rightArr);
+
+  const merged = [];
+  let leftArrIdx = 0;
+  let rightArrIdx = 0;
+
+  while (leftArrIdx < leftSortedArr.length || rightArrIdx < rightSortedArr.length) {
+    if (
+      leftArrIdx >= leftSortedArr.length ||
+      leftSortedArr[leftArrIdx] > rightSortedArr[rightArrIdx]) {
+      merged.push(rightSortedArr[rightArrIdx]);
+      rightArrIdx++;
+    } else {
+      merged.push(leftSortedArr[leftArrIdx]);
+      leftArrIdx++;
+    }
+  }
+
+  return merged;
 }
-
-function merge(left, right){
-
-}
-
-
-const answer = mergeSort(numbers);
-console.log(answer);
