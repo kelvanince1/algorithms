21a2f24d61fed4daa7efc8acb6e4052b7183c667
Kelvan Ince <incekelvan@gmail.com>
2020-05-26 19:53:50 -0400

    Add Balance Point algorithm

---
+++ Arrays&Strings/balancePoint/script.js
@@ -0,0 +1,25 @@
+function balancePoints(array) {
+    if (!Array.isArray(array)) {
+      return [];
+    }
+  
+    var totalSum = array.reduce((prev, curr) => {
+      return prev + curr;
+    });
+    console.log('TOTAL SUM', totalSum);
+    var leftSum = 0;
+  
+    return array.reduce(function(points, current, i) {
+      if (i > 0) {
+          leftSum += array[i-1];
+      }
+     
+      var rightSum = totalSum - leftSum - current;
+      console.log('RIGHT SUM', rightSum);
+      if (leftSum === rightSum) {
+        points.push(i);
+      }
+  
+      return points;
+    }, []);
+  }
