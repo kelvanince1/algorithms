278d5c4b3bb29a582fbf14c9dc2d4d51c3069f68
Kelvan Ince <incekelvan@gmail.com>
2020-05-31 15:49:42 -0400

    Add brute force and sliding window solutions to longest substring problem set

--- Arrays&Strings/longestSubstring/index.js
+++ Arrays&Strings/longestSubstring/index.js
@@ -3,11 +3,56 @@
 
 function lengthOfLongestSubstring(s) {
     const obj = {};
-    var counter = 0;
+    var counter = 1;
     
     return s.split('').reduce((max, v, i) => {
         counter = obj[v] >= counter ? obj[v] + 1 : counter;
         obj[v] = i;
         return Math.max(max, i - counter + 1);
     }, 0);
+}
+
+// Brute force
+function longestSubStr(str) {
+    let counter = 0;
+
+    for (let i = 0; i < str.length; i++) {
+        let char = str.charAt(i);
+        let set = new Set([char]);
+
+        for (let j = i + 1; j < str.length; j++) {
+            let char = str.charAt(j);
+
+            if (set.has(char)) {
+                break;
+            } else {
+                set.add(char);
+            }
+
+            counter = Math.max(counter, set.size);
+        }
+    }
+}
+
+// Sliding Window
+function longestSubString(str) {
+    let counter = 0;
+    let i = 0;
+    let j = 0;
+    let set = new Set([]);
+
+    while (i < str.length && j < str.length) {
+        let char = str.chatAt(j);
+
+        if (!set.has(char)) {
+            set.add(char);
+            j++;
+            counter = Math.max(counter, j - i);
+        } else {
+            set.delete(s.charAt(i));
+            i++;
+        }
+    }
+
+    return counter;
 }
