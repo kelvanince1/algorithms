04e4c88abe2bef085ce52fbd742e72e64bff5bd9
Kelvan Ince <incekelvan@gmail.com>
2020-06-07 08:56:44 -0400

    Add valid mountain array

---
+++ Arrays&Strings/validMountain/script.js
@@ -0,0 +1,40 @@
+// Given an array A of integers, return true if and only if it is a valid mountain 
+// array.
+
+// Recall that A is a mountain array if and only if:
+
+// A.length >= 3
+// There exists some i with 0 < i < A.length - 1 such that:
+// A[0] < A[1] < ... A[i-1] < A[i]
+// A[i] > A[i+1] > ... > A[A.length - 1]
+
+function mountainArr(arr) {
+    let max = 0;
+    let idx = 0;
+    let prevEl = arr[0];
+    
+    for (let j = 0; j < arr.length; j++) {
+        if (arr[j] > max) {
+            max = arr[j];
+            idx = j;
+        }
+    }
+
+    if (idx === 0) {
+        return false;
+    }
+
+    for (let i = 1; i < arr.length; i++) {
+        if (i < idx && prevEl >= arr[i]) {
+           return false;
+        }
+
+        if (i > idx && prevEl <= arr[i]) {
+            return false;
+        }
+
+        prevEl = arr[i];
+    }
+
+    return true;
+}
