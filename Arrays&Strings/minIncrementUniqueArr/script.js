a322785f751da53c179121eeb3b01017563ae800
Kelvan Ince <incekelvan@gmail.com>
2020-06-07 10:56:23 -0400

    Add algorithm to return number needed to make array unique

---
+++ Arrays&Strings/minIncrementUniqueArr/script.js
@@ -0,0 +1,52 @@
+// Given an array of integers A, a move consists of choosing any A[i], and 
+// incrementing it by 1.
+
+// Return the least number of moves to make every value in A unique.
+
+ 
+
+// Example 1:
+
+// Input: [1,2,2]
+// Output: 1
+// Explanation:  After 1 move, the array could be [1, 2, 3].
+// Example 2:
+
+// Input: [3,2,1,2,1,7]
+// Output: 6
+// Explanation:  After 6 moves, the array could be [3, 4, 1, 2, 5, 7].
+// It can be shown with 5 or less moves that it is impossible for the array to have 
+// all unique values.
+
+function minIncrement(arr) {
+    const obj = {};
+    let counter = 0;
+    
+    for (let ele of arr) {
+        if (obj[ele]) {
+            obj[ele]++;
+        } else {
+            obj[ele] = 1;
+        }
+    }
+    
+    for (let i = 0; i < arr.length; i++) {
+        let curr = arr[i];
+        if (obj[arr[i]] > 1) {
+            let checker = true;
+            while (checker) {
+                if (!obj[curr]) {
+                    obj[curr] = 1;
+                    checker = false;
+                } else {
+                    curr++;
+                    counter++;
+                }
+            }
+            obj[arr[i]]--;
+            arr[i] = curr;
+        }
+    }
+    
+    return counter;
+}
